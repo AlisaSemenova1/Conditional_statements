36957b1eeefe7fc8541b4ecd212371ad0ca9e9fc
Alisa Semenova <salisa4227@gmail.com>
2022-11-29 23:51:37 +0300

    Условные операторы.

--- src/main/java/TimesOfDay.java
+++ src/main/java/TimesOfDay.java
@@ -1,17 +1,17 @@
 public class TimesOfDay {
     public static void main(String[] args) {
-        timesOfDay(20);
+        timesOfDay(5);
 
     }
 
     static int timesOfDay(int time) {
-        if (time > 4 && time < 11) {
+        if (time > 4 && time <= 11) {
             System.out.println("Утро");
-        } else if (time > 11 && time < 16) {
+        } else if (time > 11 && time <= 16) {
             System.out.println("День");
         } else if (time > 17 && time <= 23) {
             System.out.println("Вечер");
-        } else if (time < 5) {
+        } else if (time <= 5) {
             System.out.println("Ночь");
         }
         return time;
